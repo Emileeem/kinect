134189621c7eec200fdbb5b07b70cfa257cec73b
Mekafuyu <mayconbrunob@hotmail.com>
2023-06-21 20:54:16 -0300

    Rebased Lima

--- Lima/Lima.cs
+++ Lima/Lima.cs
@@ -1,1 +1,33 @@
-namespace Lima;
+using System.Drawing;
+
+namespace Lima; 
+
+class Histogram
+{
+    public int[] histogram = null;
+
+    public Histogram(Bitmap bmp)
+    {
+        this.histogram = genHistogram(bmp);
+    }
+
+    public int[] genHistogram(Bitmap bmp)
+    {
+        int[] hist = new int[256];
+
+        for (int y = 0; y < bmp.Height; y++)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                Color pixel = bmp.GetPixel(x, y);
+                int intensidade = (int)(pixel.R * 0.299 + pixel.G * 0.587 + pixel.B * 0.114);
+                hist[intensidade]++;
+            }
+        }
+
+        return hist;
+    }
+}
+
+
+
