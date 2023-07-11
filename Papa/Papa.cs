7a020fab649becd00fc8f4c12899bd9763e23842
Mekafuyu <mayconbrunob@hotmail.com>
2023-07-11 14:27:47 -0300

    Transforming to static class

--- Papa/Papa.cs
+++ Papa/Papa.cs
@@ -2,7 +2,7 @@
 
 namespace Papa
 {
-    public class Plano
+    public static class Plano
     {
         /// <summary>
         /// Função para calcular o vetor entre dois pontos p e q
@@ -10,7 +10,7 @@ namespace Papa
         /// <param name="p">Ponto P</param>
         /// <param name="q">Ponto Q</param>
         /// <returns>Retorna um array contendo os componentes do vetor</returns>
-        public (float a, float b, float c) CalcularVetor((float x, float y, float z) p, (float x, float y, float z) q)
+        public static (float a, float b, float c) CalcularVetor((float x, float y, float z) p, (float x, float y, float z) q)
         {
             // Calcula as diferenças nas coordenadas entre p e q para obter as componentes do vetor
             float vetorX = q.x - p.x;
@@ -26,7 +26,7 @@ namespace Papa
         /// <param name="p">Ponto P</param>
         /// <param name="q">Ponto Q</param>
         /// <returns>Retorna ponto médio em abc</returns>
-        public (float a, float b, float c) CalcularPontoMedio((float x, float y, float z) p, (float x, float y, float z) q)
+        public static (float a, float b, float c) CalcularPontoMedio((float x, float y, float z) p, (float x, float y, float z) q)
         {
             var vetor = CalcularVetor(p, q); //usa a função CalcularVetor para obter o vetor entre dois pontos (p,q), que retorna as direções a, b e c do vetor
             float pontoMedioA = p.x + vetor.a / 2; //divide esse vetor por 2 para encontrar o ponto médio entre os dois pontos.
@@ -42,7 +42,7 @@ namespace Papa
         /// <param name="p">Ponto P</param>
         /// <param name="q">Ponto Q</param>
         /// <returns>Retorna as coordenadas do plano, sendo abcd</returns>
-        public (float a, float b, float c, float d) CalcularPlano((float x, float y, float z) p, (float x, float y, float z) q)
+        public static (float a, float b, float c, float d) CalcularPlano((float x, float y, float z) p, (float x, float y, float z) q)
         {
             var vetor = CalcularVetor(p, q);
             var PontoMedio = CalcularPontoMedio(p, q);  // ponto C (x, y, z) que é a media entre A e B
